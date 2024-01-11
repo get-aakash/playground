b5400a5907ff20b45dc890b81184d45e21315171
Aakash Acharya <mail.aakash108@gmail.com>
2024-01-11 22:38:05 +1100

    setting up file for the todo app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import {  Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import SearchForm from './components/SearchForm';
-import { useState } from 'react';
-import MyMovieList from './components/MyMovieList';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import Dashboard from './pages/Dashboard';
+
 
 
 function App() {
- const [movieList, setMovieList] = useState([])
- console.log(movieList)
- const addMovie = (movie)=>{
-  setMovieList([...movieList, movie])
-    
-
- }
+ 
   return (
-    <div className="wrapper bg-dark pt-5">
-      
-      <Container>
-        <h3 className='text-center'>My Movie Collection</h3>
-        <hr />
-        <SearchForm addMovie={addMovie} />
-        <MyMovieList data={movieList} />
-      </Container>
+    <div className="app">
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/register' element={<Registration />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+
+    </Routes>
       
     </div>
   );
